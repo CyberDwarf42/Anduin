b3110e66c07e9acf092b6cb2fe2c584aae6e7b95
Aaron Gockley <genesisfan1@gmail.com>
2024-11-16 08:17:49 -0500

    Feature Complete
    Feature Complete Project, customer history prints properly.

--- historyprint.php
+++ historyprint.php
@@ -1,18 +1,14 @@
-<!--Aaron Gockley
-11/15/2024
-SDEV-435-81
-Argonath Inventory Management Systems
-This page creates a pdf to print or email to the customer with order history->
 <?php
 
 $id = $_GET['ID'];
 use Dompdf\Dompdf;
+$url="http://localhost/Anduin/customerhistory.php?ID=$id";
 
 
 require_once('dompdf/autoload.inc.php');
 
-$dompdf1 = new Dompdf;
-$dompdf1->loadHtml(file_get_contents('http://localhost/Anduin/customerhistory.php?ID=1'));
-$dompdf1->setPaper('A4', 'portrait');
-$dompdf1->render();
-$dompdf1->stream("Customer #$id.pdf", array("Attachment" => false));
+$dompdf = new Dompdf;
+$dompdf->loadHtml(file_get_contents($url));
+$dompdf->setPaper('A4', 'portrait');
+$dompdf->render();
+$dompdf->stream("Customer #$id.pdf", array("Attachment" => false));
--- printorder.php
+++ printorder.php
@@ -1,15 +1,14 @@
 <?php
 
-//$id = $_GET['ID'];
+$id = $_GET['ID'];
 use Dompdf\Dompdf;
 
 
 require_once('dompdf/autoload.inc.php');
 
-//'http://localhost/Anduin/Order.php?ID='.$id
 
 $dompdf = new Dompdf;
-$dompdf->loadHtml(file_get_contents('http://localhost/Anduin/customerhistory.php?ID=1'));
+$dompdf->loadHtml(file_get_contents('http://localhost/Anduin/Order.php?ID='.$id));
 $dompdf->setPaper('A4', 'portrait');
 $dompdf->render();
 $dompdf->stream("Order #1.pdf", array("Attachment" => false));

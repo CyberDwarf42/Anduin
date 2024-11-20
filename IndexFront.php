ce61a6e4cd85ae55c2a89eacfa3acc1e06468d54
Aaron Gockley <genesisfan1@gmail.com>
2024-11-19 19:39:52 -0500

    IndexFrontV2
    Rewrote display loop to use execute_query and a foreach instead of what I was doing, looks much cleaner.

--- IndexFront.php
+++ IndexFront.php
@@ -4,45 +4,40 @@ SDEV-435-81
 Argonath Inventory Management Systems
 This page displays all the inventory items on the storefront-->
 
+
+
+
 <?php
 session_start();
 include 'utilities.php';
-$Link = OpenConn();
+$connection = OpenConn();
 front_header("Storefront")?>
 <div class = "container">
     <?php
 
 
 
-    $Query = "SELECT * FROM inventory";
+    $result = $connection->execute_query("SELECT * FROM inventory");
 
-    if ($result = mysqli_query($Link, $Query)) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $Name = $row['Name'];
-            $Price = $row['Price'];
-            $QtyOnHand = $row['QtyOnHand'];
-            $QtyCommitted = $row['QtyCommitted'];
-            $Image = $row['ImagePath'];
+    $inventory = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    foreach ($inventory as $inventory_item):
 
             /**
              * I used this type of container to display the information so that I could make it look better. This was also a test for the actual store page layout.
              */
-
             ?>
             <div class = "inventory-container">
                 <div class="image">
-                    <img src="<?php echo $Image; ?>">
+                    <img src="<?php echo $inventory_item['ImagePath']; ?>">
                 </div>
                 <div class="Information">
-                    <h5><?php echo "<a href='Product.php?Name=$Name'>" .$Name. '</a>'?></h5> <!--This lets the name be a link, which will transfer the name information to the Update.php page -->
-                    <h5><?php echo "$".$Price ?></h5>
-                    <h5><?php echo "On Hand: " .$QtyOnHand?></h5>
+                    <h5><a href="Product.php?Name=<?php echo $inventory_item['Name'];?>"><?php echo $inventory_item['Name'] ?></a></h5> <!--This lets the name be a link, which will transfer the name information to the Update.php page -->
+                    <h5><?php echo "$".$inventory_item['Price'] ?></h5>
+                    <h5><?php echo "On Hand: " .$inventory_item['QtyOnHand']?></h5>
                 </div>
             </div>
-            <?php
-        }
-    }
-    mysqli_close($Link);
+            <?php endforeach;
+    mysqli_close($connection);
     ?>
 </div>
 <?php front_footer()?>

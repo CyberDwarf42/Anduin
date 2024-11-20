cb3c26791fee75027f15c8da2f8b55411d0046c9
Aaron Gockley <genesisfan1@gmail.com>
2024-11-19 19:25:44 -0500

    Thru historyprint
    Added a close connection to customerhistory.

--- customerhistory.php
+++ customerhistory.php
@@ -6,7 +6,9 @@ include 'utilities.php';
 $connection = OpenConn();
 
 $lines = $connection->execute_query("SELECT orderids.OrderID, lineitems.Qty, inventory.Name FROM orderids INNER JOIN lineitems ON orderids.OrderID = lineitems.OrderID INNER JOIN inventory ON lineitems.Item = inventory.ID WHERE orderids.Customer = '$ID'");
+//no need for parameterized query, since the ID is coming directly from the database, based on previous page.
 
+mysqli_close($connection); //closed connection since it is no longer needed.
 ?>
 
 <table>
